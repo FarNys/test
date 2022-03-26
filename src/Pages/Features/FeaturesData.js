f6e1c3452c3a2d54ee15c2377c9a3082c8a431af
FarNys <faridsalmaniy@gmail.com>
2022-03-26 06:39:03 -0700

    add features-data-animation-svg+content+animation +scroll animation with gsap for triangles +css declare animation effect

--- src/Pages/Features/FeaturesData.js
+++ src/Pages/Features/FeaturesData.js
@@ -1,77 +1,229 @@
 import React, { useEffect, useLayoutEffect } from "react";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { duration } from "@mui/material";
+import { Fade, Slide } from "react-awesome-reveal";
+
 const FeaturesData = () => {
-  // gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-  // useLayoutEffect(() => {
-  //   const getEl = document.querySelector(".feature_scroll_card");
-  //   console.log(getEl.clientHeight);
-  //   gsap
-  //     .timeline({
-  //       scrollTrigger: {
-  //         trigger: ".features_data_inner",
-  //         start: "top top",
-  //         end: "bottom center",
-  //         // markers: true,
-  //         scrub: true,
-  //         pin: true,
-  //       },
-  //     })
-  //     .to(".features_data_child_img_2", 1, { opacity: 1 })
-  //     .to(".features_data_card_1", 1, { opacity: 1 })
-  //     .to(".features_data_card_2", 1, { opacity: 1 });
-  // }, []);
-  // useLayoutEffect(() => {
-  //   const getEl = document.querySelector(".stick");
-  //   const getBoundary = window.pageYOffset + getEl.getBoundingClientRect().top;
-  //   const getTarget = document.querySelector(".features_data_img_container");
-  //   // const func = () => {
-  //   //   console.log(getEl, getBoundary, window.scrollY);
-  //   // };
-  //   window.addEventListener("scroll", () => {
-  //     if (getBoundary > window.scrollY) {
-  //       console.log("HI all");
-  //       console.log(getBoundary, window.scrollY);
-  //     }
-  //     if (getBoundary + 250 < window.scrollY) {
-  //       getTarget.style.transform = "translateX(200px)";
-  //     } else {
-  //       getTarget.style.transform = "translateX(0)";
-  //     }
-  //   });
+  //ADD GSAP SCROLL EFFECT FOR ANIMATING TRIANGLES
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".feature_data_shape_1",
+          start: "top center",
+          end: "bottom top",
+          // markers: true,
+          scrub: true,
+          // pan: true,
+        },
+      })
+      .to(".feature_data_shape_1", 3, { y: 500 });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".feature_data_shape_2",
+          start: "top center",
+          end: "bottom top",
+          // markers: true,
+          scrub: true,
+          // pan: true,
+        },
+      })
+      .to(".feature_data_shape_2", 3, { y: -250 });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".feature_data_shape_3",
+          start: "top center",
+          end: "bottom top",
+          // markers: true,
+          scrub: true,
+          // pan: true,
+        },
+      })
+      .to(".feature_data_shape_3", 3, { y: 500 });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".feature_data_shape_4",
+          start: "top center",
+          end: "bottom top",
+          // markers: true,
+          scrub: true,
+          // pan: true,
+        },
+      })
+      .to(".feature_data_shape_4", 3, { y: -250 });
+  }, []);
+  //
 
-  // }, []);
+  //ADD ANIMATION EFFECT TO ANIMATE SVG IMAGES
+  useLayoutEffect(() => {
+    //SVG IMAGE SRC
+    const imgSrc = [
+      "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step1-2D_aeea862f0b.svg",
+      "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step2-2D_6729998af1.svg",
+      "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step3-2D_7bc731eae8.svg",
+      "https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step5-2D_9d3fc362cc.svg",
+    ];
+    //VARIABLE TO GET ELEMENTS AND CURRENT DISTANCE OF ELEMENT FROM TOP OF THE PAGE
+    const getTarget = document.querySelector(".features_data_img_container");
+    const getParentImg = document.querySelector(".features_data_parent_img");
+    const getChildImg = document.querySelector(".features_data_child_img");
+    const getContent = document.querySelector(".features_data_inner");
+    const gridHeight = getContent.offsetTop;
+
+    //ADD LISTENER TO ADD SCROLL EFFECT
+    window.addEventListener("scroll", () => {
+      //CURRENT SCROLL POSITION + CURRENT HEIGHT OF DISPLAY (PX)
+      const currScroll = window.scrollY;
+      const currViewHeight = document.documentElement.clientHeight;
+
+      //CONDITION TO CHANGE ANIMATION NAME AND IMG SRC
+      if (gridHeight + currViewHeight > currScroll) {
+        getChildImg.src = imgSrc[0];
+        getChildImg.style.transformOrigin = "bottom right";
+        getParentImg.style.animation =
+          "img-parent-animate-left 0.5s ease forwards";
+        getChildImg.style.animation =
+          "img-child-animate-left 0.5s ease forwards";
+      }
+      if (gridHeight + currViewHeight < currScroll) {
+        getChildImg.src = imgSrc[1];
+        getChildImg.style.transformOrigin = "bottom center";
+        getParentImg.style.animation =
+          "img-parent-animate-right 0.5s ease forwards";
+        getChildImg.style.animation =
+          "img-child-animate-right 0.5s ease forwards";
+      }
+      if (
+        gridHeight + currViewHeight * 2 < currScroll &&
+        gridHeight + currViewHeight * 3 > currScroll
+      ) {
+        getChildImg.style.transformOrigin = "bottom right";
+        getChildImg.src = imgSrc[2];
+        getParentImg.style.animation =
+          "img-parent-animate-left 0.5s ease forwards";
+        getChildImg.style.animation =
+          "img-child-animate-left 0.5s ease forwards";
+      }
+      if (
+        gridHeight + currViewHeight * 3 < currScroll &&
+        gridHeight + currViewHeight * 4 > currScroll
+      ) {
+        getChildImg.style.transformOrigin = "bottom center";
+        getChildImg.src = imgSrc[3];
+        getParentImg.style.animation =
+          "img-parent-animate-right 0.5s ease forwards";
+        getChildImg.style.animation =
+          "img-child-animate-right 0.5s ease forwards";
+      }
+
+      console.log(gridHeight, currScroll, currViewHeight);
+    });
+  }, []);
+
   return (
     <div className="features_data_container">
-      <div className="stick">TEST STICKY</div>
+      <div className="features_data_spacer"></div>
+      <div className="features_data_img_container">
+        <img
+          className="features_data_parent_img"
+          src="https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-primary-desktop_e0dab1f509.svg"
+          alt="svg"
+        />
+        <img
+          className="features_data_child_img"
+          src="https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step5-2D_9d3fc362cc.svg"
+          alt="svg-1"
+        />
+      </div>
 
       <div className="features_data_inner">
-        <div className="features_data_img_container">
-          <img
-            className="features_data_parent_img"
-            src="https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-primary-desktop_e0dab1f509.svg"
-            alt="svg"
-          />
-          <img
-            className="features_data_child_img_1"
-            src="https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step5-2D_9d3fc362cc.svg"
-            alt="svg-1"
-          />
-          <img
-            className="features_data_child_img_2"
-            src="https://d2zv2ciw0ln4h1.cloudfront.net/uploads/hp-step1-2D_aeea862f0b.svg"
-            alt="svg-2"
-          />
+        <div className="features_data_grid_content">
+          <div className="features_data_grid_content_shape_left feature_data_shape_1"></div>
         </div>
-        <div className="features_data_text_container">
-          <div className="features_data_card features_data_card_1">TEXT 1</div>
-          <div className="features_data_card features_data_card_2">TEXT 2</div>
-          <div className="features_data_card features_data_card_3"> TEXT 3</div>
+        <div className="features_data_grid_content">
+          <Slide direction="up">
+            <Fade delay={250}>
+              <h5>Step 1</h5>
+              <h4>
+                Easily build apps and digital experiences without the
+                distraction of CMS complexities.
+              </h4>
+              <p>
+                Focus your talent on what matters. You can integrate individual
+                best-in-class services by third-party vendors and connect any
+                framework of your choice.
+              </p>
+            </Fade>
+          </Slide>
+        </div>
+        <div className="features_data_grid_content">
+          {" "}
+          <Slide direction="up">
+            <Fade delay={250}>
+              <h5>Step 2</h5>
+              <h4>
+                Easily build apps and digital experiences without the
+                distraction of CMS complexities.
+              </h4>
+              <p>
+                Focus your talent on what matters. You can integrate individual
+                best-in-class services by third-party vendors and connect any
+                framework of your choice.
+              </p>
+            </Fade>
+          </Slide>
+        </div>
+        <div className="features_data_grid_content">
+          {" "}
+          <div className="features_data_grid_content_shape_right feature_data_shape_2"></div>
+        </div>
+        <div className="features_data_grid_content">
+          {" "}
+          <div className="features_data_grid_content_shape_left feature_data_shape_3"></div>
+        </div>
+        <div className="features_data_grid_content">
+          {" "}
+          <Slide direction="up">
+            <Fade delay={250}>
+              <h5>Step 3</h5>
+              <h4>
+                Easily build apps and digital experiences without the
+                distraction of CMS complexities.
+              </h4>
+              <p>
+                Focus your talent on what matters. You can integrate individual
+                best-in-class services by third-party vendors and connect any
+                framework of your choice.
+              </p>
+            </Fade>
+          </Slide>
+        </div>
+        <div className="features_data_grid_content">
+          <Slide direction="up">
+            <Fade delay={250}>
+              <h5>Step 4</h5>
+              <h4>
+                Easily build apps and digital experiences without the
+                distraction of CMS complexities.
+              </h4>
+              <p>
+                Focus your talent on what matters. You can integrate individual
+                best-in-class services by third-party vendors and connect any
+                framework of your choice.
+              </p>
+            </Fade>
+          </Slide>
+        </div>
+        <div className="features_data_grid_content">
+          {" "}
+          <div className="features_data_grid_content_shape_right feature_data_shape_4"></div>
         </div>
       </div>
-      <div className="extra">Extra</div>
     </div>
   );
 };
